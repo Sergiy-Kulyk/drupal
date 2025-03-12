ee61ccd04cc952174db7240638b7c6673f2e8233
Serhii Kulyk <skulyk@alphawebgroup.com>
2025-03-12 14:42:26 +0800

    Added logo from umami.

--- ctnext/components/navigation/HeaderNav.tsx
+++ ctnext/components/navigation/HeaderNav.tsx
@@ -1,9 +1,13 @@
 import { Link } from "@/components/navigation/Link"
+import { Logo } from "@/components/blocks/logo";
 
 export function HeaderNav() {
   return (
     <header>
       <div className="container flex items-center justify-between py-6 mx-auto">
+        <div className="logo">
+          <Logo className="w-48 h-12 text-primary lg:h-16 lg:w-52" />
+        </div>
         <Link href="/" className="text-2xl font-semibold no-underline">
           Next.js for Drupal
         </Link>
